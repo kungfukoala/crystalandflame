761c34a2de61afed9486760488dea68010b48c22
Benjamin Kinzer <ben@benjaminkinzer.com>
2024-10-02 21:09:41 -0700

    Build Change
    Change Package.json build

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '35538b0f7a96830534e5f03f95eaeaa932d4823a', queries,  });
 export default client;
   
