dfc44f4c16c9b5d3fb046bf21750e924ea5c646a
Benjamin Kinzer <ben@benjaminkinzer.com>
2024-10-09 21:27:38 -0700

    MDX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        react(),
-        tailwind({
-            applyBaseStyles: false
-        })
-    ],
+    integrations: [react(), tailwind({
+        applyBaseStyles: false
+    }), mdx()],
     output: 'hybrid',
     adapter: netlify()
 });
